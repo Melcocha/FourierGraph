fe95e5a12ad23a4266bbe64800dcf16c9df25501
Melcocha <mr.arteaga06@gmail.com>
2024-04-16 00:10:48 -0600

    Cambios 15-04-2024

--- app/src/main/java/com/example/fouriergraph/MainActivity.java
+++ app/src/main/java/com/example/fouriergraph/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fouriergraph;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        setTheme(R.style.Splashtheme);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.opciones_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void irAGraficador(View view) {
+        Intent intent = new Intent(this, Graph.class);
+        startActivity(intent);
     }
 }
