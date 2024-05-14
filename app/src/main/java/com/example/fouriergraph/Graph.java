631414686e9b7d4b771f9794e46063a3f4cbb2c0
dsantacruz1212 <danielsantacruz.1212@gmail.com>
2024-05-13 22:24:30 -0600

    Proyecto graficando

--- app/src/main/java/com/example/fouriergraph/Graph.java
+++ app/src/main/java/com/example/fouriergraph/Graph.java
@@ -5,16 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.graphics.Bitmap;
 import android.widget.ImageView;
-import com.example.fouriergraph.GraphView;
+import android.util.DisplayMetrics;
+
 import java.util.ArrayList;
 import java.lang.Math;
 import android.view.ViewGroup;
 
-import android.content.Context;
+
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.view.View;
+
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -82,6 +83,14 @@ public class Graph extends AppCompatActivity {
                     case "Onda cuadrada":
                         ArrayList<double[]> puntosCuadrado = generarOndaCuadrada(ondaDouble);
                         ArrayList<double[]> puntosCuadradoFourier = generarOndaCuadradaFourier(ondaDouble, armonicoDouble);
+                        for (double[] punto : puntosCuadrado) {
+                            Log.i("Onda sinusoidal", "Tiempo: " + punto[0] + ", Valor: " + punto[1]);
+                        }
+
+                        Log.i("Onda cuadrada", "Valores de la transformada de Fourier de la onda sinusoidal rectificada:");
+                        for (double[] punto : puntosCuadradoFourier) {
+                            Log.i("Onda cuadrada", "Tiempo: " + punto[0] + ", Valor: " + punto[1]);
+                        }
                         Graficar(puntosCuadrado, puntosCuadradoFourier);
 
                         break;
@@ -95,14 +104,14 @@ public class Graph extends AppCompatActivity {
                     case "Forma de onda triangular":
                         ArrayList<double[]> puntosTriangular = generarOndaTriangular(ondaDouble);
                         ArrayList<double[]> puntosTriangularFourier = generarOndaTriangularFourier(ondaDouble, armonicoDouble);
-
+                        Graficar(puntosTriangular, puntosTriangularFourier);
 
                         break;
 
                     case "Forma de onda de diente de sierra":
                         ArrayList<double[]> puntosSierra = generarOndaDienteSierra(ondaDouble);
                         ArrayList<double[]> puntosSierraFourier = generarOndaDienteSierraFourier(ondaDouble, armonicoDouble);
-
+                        Graficar(puntosSierra, puntosSierraFourier);
                         break;
 
 
@@ -583,71 +592,68 @@ public class Graph extends AppCompatActivity {
     }
 
     public void Graficar(ArrayList<double[]> GraficaOriginal, ArrayList<double[]> GraficaFourier) {
-
         Paint paint = new Paint();
         paint.setColor(Color.RED);
         paint.setStrokeWidth(5);
 
-
         ImageView imageView = findViewById(R.id.imageView);
 
-
         if (imageView != null) {
-
-            int imageViewWidth = 383;
-            int imageViewHeight = 518;
-
-
-            Log.d("PRUEBA", "Ancho del ImageView: " + imageViewWidth);
-            Log.d("PRUEBA", "Alto del ImageView: " + imageViewHeight);
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            int screenWidth = displayMetrics.widthPixels;
 
 
-            Log.d("PRUEBA", "Estableciendo dimensiones al ImageView");
-            ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
-            layoutParams.width = imageViewWidth;
-            layoutParams.height = imageViewHeight;
-            imageView.setLayoutParams(layoutParams);
+            int originalWidth = screenWidth;
+            int originalHeight = 200;
+
+            Bitmap bitmap = Bitmap.createBitmap(displayMetrics.widthPixels, 700, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            int offsetX = 0;
+            int offsetY = -30;
+            int newWidth = bitmap.getWidth();
+            int newHeight = bitmap.getHeight() + offsetY;
 
 
-            if (imageViewWidth > 0 && imageViewHeight > 0) {
-                // Crear un nuevo Bitmap con las dimensiones del ImageView para dibujar la gráfica
-                Log.d("PRUEBA", "Creando Bitmap con dimensiones del ImageView");
-                Bitmap bitmap = Bitmap.createBitmap(imageViewWidth, imageViewHeight, Bitmap.Config.ARGB_8888);
-                bitmap.setDensity(getResources().getDisplayMetrics().densityDpi);
-
-                Canvas canvas = new Canvas(bitmap);
-
-
-                Log.d("PRUEBA", "Dibujando gráfica original");
-                for (int i = 0; i < GraficaOriginal.size() - 1; i++) {
-                    double[] puntoInicio = GraficaOriginal.get(i);
-                    double[] puntoFin = GraficaOriginal.get(i + 1);
-                    Log.d("PRUEBA", "Dibujando línea de (" + puntoInicio[0] + ", " + puntoInicio[1] + ") a (" + puntoFin[0] + ", " + puntoFin[1] + ")");
-                    canvas.drawLine((float) puntoInicio[0], (float) puntoInicio[1], (float) puntoFin[0], (float) puntoFin[1], paint);
-                }
-
-
-                paint.setColor(Color.BLUE);
-
-
-                Log.d("PRUEBA", "Dibujando gráfica de Fourier");
-                for (int i = 0; i < GraficaFourier.size() - 1; i++) {
-                    double[] puntoInicio = GraficaFourier.get(i);
-                    double[] puntoFin = GraficaFourier.get(i + 1);
-                    Log.d("PRUEBA", "Dibujando línea de (" + puntoInicio[0] + ", " + puntoInicio[1] + ") a (" + puntoFin[0] + ", " + puntoFin[1] + ")");
-                    canvas.drawLine((float) puntoInicio[0], (float) puntoInicio[1], (float) puntoFin[0], (float) puntoFin[1], paint);
-                }
-
-
-                Log.d("PRUEBA", "Asignando Bitmap al ImageView");
-                imageView.setImageBitmap(bitmap);
-            } else {
-                Log.e("PRUEBA", "Ancho o alto del ImageView no válido.");
+            paint.setColor(Color.RED);
+            Log.d("PRUEBA", "Dibujando gráfica original");
+            for (int i = 0; i < GraficaOriginal.size() - 1; i++) {
+                double[] puntoInicio = GraficaOriginal.get(i);
+                double[] puntoFin = GraficaOriginal.get(i + 1);
+                float xInicio = (float) puntoInicio[0] * originalWidth/20 ;
+                float yInicio = (float) puntoInicio[1] *originalHeight ;
+                float xFin = (float) puntoFin[0] *originalWidth/20 ;
+                float yFin = (float) puntoFin[1]  *originalHeight;
+                Log.d("PRUEBA", "Dibujando línea de (" + xInicio + ", " + yInicio + ") a (" + xFin + ", " + yFin + ")");
+                canvas.drawLine(xInicio, yInicio+400, xFin, yFin+400, paint);
             }
+
+            paint.setColor(Color.GREEN);
+            Log.d("PRUEBA", "Dibujando gráfica de Fourier");
+            for (int i = 0; i < GraficaFourier.size() - 1; i++) {
+                double[] puntoInicio = GraficaFourier.get(i);
+                double[] puntoFin = GraficaFourier.get(i + 1);
+                float xInicio = (float) puntoInicio[0]*originalWidth/20 ;
+                float yInicio = (float) puntoInicio[1]*originalHeight ;
+                float xFin = (float) puntoFin[0]*originalWidth/20 ;
+                float yFin = (float) puntoFin[1] * originalHeight ;
+                Log.d("PRUEBA", "Dibujando línea de (" + xInicio + ", " + yInicio + ") a (" + xFin + ", " + yFin + ")");
+                canvas.drawLine(xInicio, yInicio+400, xFin, yFin+400, paint);
+            }
+
+            Bitmap shiftedBitmap = Bitmap.createBitmap(newWidth, newHeight, bitmap.getConfig());
+            Canvas canvas2 = new Canvas(shiftedBitmap);
+
+
+            canvas2.drawBitmap(bitmap, offsetX, offsetY, null);
+
+            Log.d("PRUEBA", "Asignando Bitmap al ImageView");
+            imageView.setImageBitmap(shiftedBitmap);
         } else {
             Log.e("PRUEBA", "ImageView es nulo. No se puede establecer la imagen.");
         }
     }
+
 
 
 
